eb08d0d55374b1c5d7dd81b40cd7b32dce1babca
AbdelhamedG <abdelhamedg@cloudsoftway.com>
2025-01-06 12:53:39 +0200

    fixed

--- main.tf
+++ main.tf
@@ -1,5 +1,1 @@
-resource "aws_instance" "myec2" {
-  ami                     = "ami-0dcc1e21636832c5d"
-  instance_type           = "t2.micro"
-  availability_zone = "us-east-1a"
-}
+resource "null_resource" "example" {}
